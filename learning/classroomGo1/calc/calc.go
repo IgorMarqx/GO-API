c3b3b044b6272be8672cd7a05bc4c250efb38a8b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2024-01-16 21:56:54 -0300

    WIP: implementing the operation module to learn more about go

--- learning/classroomGo1/calc/calc.go
+++ learning/classroomGo1/calc/calc.go
@@ -1,22 +1,57 @@
 package calc
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Operation(operation string) int {
+func Operation(operation string) (int, error) {
 	num1 := 5
-	num2 := 2
+	num2 := 0
+	var result int
 
-	result := num1 - num2
+	if operation == "dividir" && num2 == 0 {
+		return 0, errors.New("Não é possivel dividir por 0")
+	}
 
-	if operation == "multiplicar" {
-		result = num1 * num2
-	} else if operation == "somar" {
+	if err := validation(operation); err != nil {
+		fmt.Println("A operação", operation, "não é válida")
+		return 0, err
+	}
+
+	arrayOperations := [4]string{"somar", "multiplicar", "dividir", "subtrair"}
+	var validOperation bool
+
+	for _, value := range arrayOperations {
+		if value == operation {
+			validOperation = true
+			break
+		}
+	}
+
+	if !validOperation {
+		return 0, errors.New("A operação não reconhecida")
+	}
+
+	switch operation {
+	case "somar":
 		result = num1 + num2
-	} else {
-		fmt.Println("Operação não reconhecida")
-		return 0
+	case "multiplicar":
+		result = num1 * num2
+	case "dividir":
+		result = num1 / num2
+	case "subtrair":
+		result = num1 - num2
 	}
 
 	fmt.Println("O resultado da", operation, "é", result)
-	return result
+	return result, nil
+}
+
+func validation(operation string) error {
+	if operation != "somar" && operation != "multiplicar" && operation != "dividir" && operation != "subtrair" {
+		return errors.New("A operação não é válida")
+	}
+
+	return nil
 }
--- learning/classroomGo1/main.go
+++ learning/classroomGo1/main.go
@@ -3,5 +3,5 @@ package main
 import "classroomGo1/calc"
 
 func main() {
-	calc.Operation("multiplicar")
+	calc.Operation("subtrair")
 }
